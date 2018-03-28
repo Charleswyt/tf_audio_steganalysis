ce35e6375ef4a7cef09bc15df100490a0572d893
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-03-28 16:52:01 +0800

    新增参数

--- config.py
+++ config.py
@@ -13,8 +13,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 """
 function:
-        command_parse()         : command-line arguments parse
-        file_path_setup(args)   : get the data and model files path
+        command_parse()                                     : command-line arguments parse
+        file_path_setup(args)                               : get the data and model files path
+        get_files_list(file_dir, start_idx=0, end_idx=-1)   : get files list
 """
 
 
@@ -35,6 +36,9 @@ def command_parse():
     # mode
     parser.add_argument("--mode", type=str, default="train", help="run mode -- train | test (default: train)")
     parser.add_argument("--test", type=str, default="one", help="one | batch (default one)")
+    parser.add_argument("--carrier", type=str, default="audio", help="image | audio (default audio)")
+    parser.add_argument("--network", type=str, default="network1", help="the index of the network (default: network1), "
+                                                                        "the detailed introduction of each network is in readme")
 
     # data info
     parser.add_argument("--data_dir", type=str, help="data set path")
@@ -45,8 +49,10 @@ def command_parse():
                         help="EECS | HCM-Gao | HCM- Yan (default: EECS)")
     parser.add_argument("--height", type=int, default=200, help="the height of the QMDCT matrix (default: 200)")
     parser.add_argument("--width", type=int, default=576, help="the width of the QMDCT matrix (default: 576)")
-    parser.add_argument("--start_index", type=int, default=0, help="the start index of file in train folders (default: 0)")
-    parser.add_argument("--end_index", type=int, default=10000, help="the end index of file in train folders (default: 10000)")
+    parser.add_argument("--start_index_train", type=int, default=0, help="the start index of file in train folders (default: 0)")
+    parser.add_argument("--end_index_train", type=int, default=-1, help="the end index of file in train folders (default: 16000)")
+    parser.add_argument("--start_index_valid", type=int, default=0, help="the start index of file in valid folders (default: 0)")
+    parser.add_argument("--end_index_valid", type=int, default=-1, help="the end index of file in valid folders (default: 4000)")
     parser.add_argument("--model_dir", type=str, help="model files path")
     parser.add_argument("--model_file_name", type=str, default="audio_steganalysis", help="model file name (default: audio_steganalysis)")
     parser.add_argument("--log_dir", type=str, help="log files path")
@@ -57,11 +63,15 @@ def command_parse():
     parser.add_argument("--batch_size_valid", type=int, default=128, help="batch size for valid (default: 128 (64 cover|stego pairs))")
     parser.add_argument("--epoch", type=int, default=500, help="the number of epochs for training (default: 500)")
     parser.add_argument("--learning_rate", type=float, default=1e-3, help="the value of initialized learning rate (default: 1e-3 (0.001))")
-    parser.add_argument("--gpu", type=int, default=0, help="the index of gpu used (default: 0)")
     parser.add_argument("--seed", type=int, default=1, help="random seed (default: 1)")
-    parser.add_argument("--decay_step", type=int, default=1000, help="the step for learning rate decay (default: 1000)")
-    parser.add_argument("--decay_rate", type=float, default=0.95, help="the rate for learning rate decay (default: 0.95)")
+    parser.add_argument("--is_regulation", type=bool, default=True, help="whether regulation or not (default: True)")
+
+    # learning rate parameters
+    parser.add_argument("--decay_method", type=str, default="exponential", help="the method for learning rate decay (default: exponential)")
+    parser.add_argument("--decay_step", type=int, default=5000, help="the step for learning rate decay (default: 5000)")
+    parser.add_argument("--decay_rate", type=float, default=0.9, help="the rate for learning rate decay (default: 0.95)")
     parser.add_argument("--staircase", type=bool, default=False, help="whether the decay the learning rate at discrete intervals or not (default:False)")
+
 
     # path
     parser.add_argument("--cover_train_dir", type=str,
@@ -76,8 +86,6 @@ def command_parse():
                         help="the path of directory containing models")
     parser.add_argument("--logs_path", type=str, default=current_path+"/logs",
                         help="the path of directory containing logs")
-    parser.add_argument("--network", type=str, default="network1", help="the index of the network (default: network1), "
-                                                                        "the detailed introduction of each network is in readme")
 
     # model
     parser.add_argument("--max_to_keep", type=int, default=3, help="the number needed to be saved (default: 3)")
@@ -87,7 +95,7 @@ def command_parse():
     parser.add_argument("--is_abs", type=bool, default=False, help="abs or not (default: False)")
     parser.add_argument("--is_trunc", type=bool, default=False, help="truncation or not (default: False)")
     parser.add_argument("--threshold", type=int, default=15, help="threshold (default: 15)")
-    parser.add_argument("--is_diff", type=bool, default=15, help="threshold (default: False)")
+    parser.add_argument("--is_diff", type=bool, default=False, help="threshold (default: False)")
     parser.add_argument("--order", type=int, default=2, help="the order of the difference (default: 2)")
     parser.add_argument("--direction", type=int, default=0, help="0 - row, 1 - col (default: 0)")
     parser.add_argument("--downsampling", type=bool, default=False, help="downsampling or not (default: False)")
@@ -124,25 +132,19 @@ def file_path_setup(args):
         else:
             stego_dir_name = str(bitrate) + "_" + relative_payload
 
-    cover_train_files_path = data_dir + "/" + "cover/" + str(bitrate) + "/" + "train"
-    cover_valid_files_path = data_dir + "/" + "cover/" + str(bitrate) + "/" + "valid"
-    stego_train_files_path = data_dir + "/stego/" + stego_method + "/" + stego_dir_name + "/" + "train"
-    stego_valid_files_path = data_dir + "/stego/" + stego_method + "/" + stego_dir_name + "/" + "valid"
-    model_file_path = model_dir + "/" + stego_dir_name
-    log_file_path = log_dir + "/" + stego_dir_name
-
-    # input mode
-    if args.cover_train_dir is not None:
+    if data_dir is not None:
+        cover_train_files_path = data_dir + "/" + "cover/" + str(bitrate) + "/" + "train"
+        cover_valid_files_path = data_dir + "/" + "cover/" + str(bitrate) + "/" + "valid"
+        stego_train_files_path = data_dir + "/stego/" + stego_method + "/" + stego_dir_name + "/" + "train"
+        stego_valid_files_path = data_dir + "/stego/" + stego_method + "/" + stego_dir_name + "/" + "valid"
+        model_file_path = model_dir + "/" + stego_dir_name
+        log_file_path = log_dir + "/" + stego_dir_name
+    else:
         cover_train_files_path = args.cover_train_dir
-    if args.cover_valid_dir is not None:
         cover_valid_files_path = args.cover_valid_dir
-    if args.stego_train_dir is not None:
         stego_train_files_path = args.stego_train_dir
-    if args.stego_train_dir is not None:
         stego_valid_files_path = args.stego_train_dir
-    if args.models_path is not None:
         model_file_path = args.models_path
-    if args.logs_path is not None:
         log_file_path = args.logs_path
 
     # make dir
@@ -152,3 +154,24 @@ def file_path_setup(args):
         os.mkdir(log_file_path)
 
     return cover_train_files_path, cover_valid_files_path, stego_train_files_path, stego_valid_files_path, model_file_path, log_file_path
+
+
+def get_files_list(file_dir, start_idx=0, end_idx=10000):
+    """
+    get the files list
+    :param file_dir: file directory
+    :param start_idx: start index
+    :param end_idx: end index
+    :return:
+    """
+    filename = os.listdir(file_dir)
+    file_list = [file_dir + "/" + file for file in filename]
+    total_num = len(file_list)
+    if start_idx > total_num:
+        start_idx = 0
+    if end_idx > total_num:
+        end_idx = total_num + 1
+
+    file_list = file_list[start_idx:end_idx]
+
+    return file_list
