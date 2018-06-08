823c6b8b8fd728991a4f39b4bab58182a53d5368
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-06-08 11:17:28 +0800

    modify media_read method

--- utils.py
+++ utils.py
@@ -167,30 +167,47 @@ def get_data_batch(files_list, height, width, carrier="audio", is_abs=False, is_
     return data
 
 
-def media_read(media_file_path, media_file_type, height, width, as_grey=False, sampling_rate=44100, to_mono=False,
+def media_read(media_file_path, media_file_type="text", height=200, width=576, as_grey=False, sampling_rate=44100, mono=False, offset=0, duration=None,
                is_abs=False, is_diff=False, is_diff_abs=False, order=2, direction=0, is_trunc=False, threshold=15):
     """
     read media according to the file path and file type
     :param media_file_path: the path of media file
-    :param media_file_type: the type of media file
-    :param height: the output height of media file
-    :param width: the output width of media file
+    :param media_file_type: the type of media file, default is "text"
+    :param height: the output height of media file, default is 200
+    :param width: the output width of media file, default is 576
+    :param as_grey: whether covert the image to gray image or not, default is False
+    :param sampling_rate: the sampling rate of audio file, default is 44100
+    :param mono: whether convert the audio file to mono, default is False
+     :param offset: start reading after this time (in seconds), default is 0
+    :param duration: only load up to this much audio (in seconds), default is None
+
+    # some pre-processing methods
+    :param is_abs: whether abs or not, default: False
+    :param is_diff: whether difference or not, default: False
+    :param is_diff_abs: whether abs after difference or not, default: False
+    :param order: the order of the difference, default: 2
+    :param direction: the direction of the difference (0 - row, 1 - col, default: 0)
+    :param is_trunc: whether truncation or not, default: False
+    :param threshold: threshold, default: 15
+
     :return:
         media in ndarry format
     """
     if media_file_type == "image":
         media = io.imread(media_file_path, as_grey=as_grey)
     elif media_file_type == "audio":
-        media = audio_read(media_file_path, sampling_rate=sampling_rate, to_mono=to_mono)
+        media = audio_read(media_file_path, sampling_rate=sampling_rate, mono=mono, offset=offset, duration=duration)
     elif media_file_path == "text":
-        media = text_read(media_file_path)
+        media = text_read(media_file_path, height=height, width=width)
     else:
         media = None
+
+    media = preprocess(media, is_abs=is_abs, is_diff=is_diff, is_diff_abs=is_diff_abs, order=order, direction=direction, is_trunc=is_trunc, threshold=threshold)
 
     return media
 
 
-def tfrecord_write(files_path_list, file_type, tfrecord_file_name):
+def tfrecord_write(files_path_list, tfrecord_file_name):
     writer = tf.python_io.TFRecordWriter(tfrecord_file_name)
     for index, files_path in enumerate(files_path_list):
         files_list = get_files_list(files_path)
