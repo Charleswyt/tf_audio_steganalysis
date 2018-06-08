d07874b8a2d80347d2b7090df5e42d35c12c4936
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-06-08 11:16:57 +0800

    add audioread nethod

--- audio_preprocess.py
+++ audio_preprocess.py
@@ -3,7 +3,6 @@
 
 import librosa
 import numpy as np
-import librosa.display
 import matplotlib.pyplot as plt
 
 """
@@ -19,13 +18,21 @@ function:
 """
 
 
-def audio_read(audio_file_path, sampling_rate=44100, to_mono=False):
-    audio = librosa.load(audio_file_path, sr=sampling_rate)
-    if to_mono is True:
-        audio = librosa.to_mono(audio)
+def audio_read(audio_file_path, sampling_rate=44100, mono=False, offset=0, duration=None):
+    """
+    read audio data
+    :param audio_file_path: audio file path
+    :param sampling_rate: sampling rate
+    :param mono: whether convert signal to mono, default is False
+    :param offset: start reading after this time (in seconds), default is 0
+    :param duration: only load up to this much audio (in seconds), default is None
+    :return:
+        audio data in np.float32 format
+    """
+    audio = librosa.load(audio_file_path, sr=sampling_rate, mono=mono, offset=offset, duration=duration)
 
     return audio
 
 
 if __name__ == "__main__":
-    audio_read("")
+    audio_read("E:/Myself/1.source_code/audio_steganalysis/module/test/wav10s_00001_stego_128.mp3", 44100)
