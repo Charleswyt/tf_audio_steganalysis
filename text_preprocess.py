c0dd03c5bcf33274ab02ef380f98d9bf8d5190e7
Charles_wyt <Charleswyt@users.noreply.github.com>
2018-09-19 19:41:16 +0800

    update
    修改text_read接口

--- text_preprocess.py
+++ text_preprocess.py
@@ -3,7 +3,6 @@
 
 import os
 import numpy as np
-from pre_process import preprocess
 
 """
 Created on 2017.11.27
@@ -15,14 +14,12 @@ Modified on 2018.08.23
 
 """
     function:
-        text_read(text_file_path, height=200, width=576, separator=",")                             read single QMDCT coefficients matrix into memory
-        text_read_all(text_files_dir, height=200, width=576, separator=",")                         read all QMDCT coefficients matrix read into memory
-        text_read_batch(text_files_list, height=200, width=576, separator=",")                      read QMDCT coefficients matrix in batch
+        text_read(text_file_path, height=200, width=576, separator=",")           read single QMDCT coefficients matrix into memory
+        text_read_batch(text_files_list, height=200, width=576, separator=",")    read QMDCT coefficients matrix in batch
 """
 
 
-def text_read(text_file_path, height=200, width=576, separator=",",
-              is_abs=False, is_diff=False, is_abs_diff=False, is_diff_abs=False, order=2, direction=0, is_trunc=False, threshold=15):
+def text_read(text_file_path, height=200, width=576, separator=","):
     """
     data read from one text file
 
@@ -30,14 +27,6 @@ def text_read(text_file_path, height=200, width=576, separator=",",
     :param height: the height of QMDCT matrix
     :param width: the width of QMDCT matrix
     :param separator: separator of each elements in text file
-    :param is_abs: whether make abs or not
-    :param is_diff: whether make difference or not
-    :param is_abs_diff: whether make abs and difference or not
-    :param is_diff_abs: whether make difference and abs or not
-    :param order: the order of difference
-    :param direction: the direction of difference, 0 - inter frame, 1 - intra frame
-    :param is_trunc: whether make truncation or not
-    :param threshold: the threshold of truncation
 
     :return
         content: QMDCT matrix  ndarray, shape: [height, width, 1]
@@ -53,9 +42,6 @@ def text_read(text_file_path, height=200, width=576, separator=",",
 
     content = np.array(content, dtype=np.int32)
 
-    content = preprocess(content=content, is_abs=is_abs, is_diff=is_diff, is_abs_diff=is_abs_diff, is_diff_abs=is_diff_abs,
-                         order=order, direction=direction, is_trunc=is_trunc, threshold=threshold)
-
     # reshape
     [h, w] = np.shape(content)
     content = np.reshape(content, [h, w, 1])
@@ -67,8 +53,7 @@ def text_read(text_file_path, height=200, width=576, separator=",",
     return content
 
 
-def text_read_batch(text_files_list, height=200, width=576, separator=",",
-                    is_abs=False, is_diff=False, is_abs_diff=False, is_diff_abs=False, order=2, direction=0, is_trunc=False, threshold=15):
+def text_read_batch(text_files_list, height=200, width=576, separator=","):
     """
     read all txt files into the memory
 
@@ -76,14 +61,6 @@ def text_read_batch(text_files_list, height=200, width=576, separator=",",
     :param height: the height of QMDCT matrix
     :param width: the width of QMDCT matrix
     :param separator: separator of each elements in text file
-    :param is_abs: whether make abs or not
-    :param is_diff: whether make difference or not
-    :param is_abs_diff: whether make abs and difference or not
-    :param is_diff_abs: whether make difference and abs or not
-    :param order: the order of difference
-    :param direction: the direction of difference, 0 - inter frame, 1 - intra frame
-    :param is_trunc: whether make truncation or not
-    :param threshold: the threshold of truncation
 
     :return:
         data: QMDCT matrixs, ndarry, shape: [files_num, height, width, 1]
@@ -94,8 +71,7 @@ def text_read_batch(text_files_list, height=200, width=576, separator=",",
 
     i = 0
     for text_file_path in text_files_list:
-        content = text_read(text_file_path, height=height, width=width, separator=separator, is_abs=is_abs, is_diff=is_diff, is_abs_diff=is_abs_diff,
-                            is_diff_abs=is_diff_abs, order=order, direction=direction, is_trunc=is_trunc, threshold=threshold)
+        content = text_read(text_file_path, height=height, width=width, separator=separator)
         data[i] = content
         i = i + 1
 
