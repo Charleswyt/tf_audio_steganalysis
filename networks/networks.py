b398ecf3198220e91ca5f5fbbc2dadc69dec2bb5
Charles_wyt <Charleswyt@users.noreply.github.com>
2018-09-20 00:05:13 +0800

    update
    新增networks模板，用于json文件中network参数判断

---
+++ networks/networks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2018.09.17
+Finished on 2018.09.17
+Modified on
+
+@author: Wang Yuntao
+"""
+
+
+networks = ["wasdn", "network_1", "network_2", "network_3", "network_4", "network_5", "network_6", "network_7", "network_8", "network1__1", "network1__2",
+            "network1__3", "network1__4", "downsampling_network", "downsampling_network1",
+            "le_net", "vgg16", "vgg19", "stegshi"]
